e38256613cead0d0f5ce914ce6cf7ceb17471e32
Fernando <fernandocastrejonh@gmail.com>
2025-09-29 06:29:07 -0600

    changes

--- components/InterviewForm.tsx
+++ components/InterviewForm.tsx
@@ -449,21 +449,6 @@ export default function InterviewForm({
             </div>
           )}
 
-        {/* Video Status */}
-        {videoPath && (
-          <div className="bg-green-50 dark:bg-green-900/20 border border-green-200 dark:border-green-700 rounded-lg p-4">
-            <div className="flex items-center">
-              <Video className="h-5 w-5 text-green-600 dark:text-green-400 mr-2" />
-              <span className="text-sm font-medium text-green-800 dark:text-green-200">
-                Video uploaded successfully
-              </span>
-            </div>
-            <p className="text-xs text-green-600 dark:text-green-400 mt-1 break-all">
-              {videoPath}
-            </p>
-          </div>
-        )}
-
         {/* Question Upload Section - Only show for new interviews */}
         {!initialData && (
           <div className="border-t border-gray-200 dark:border-gray-600 pt-6">
